79d3695cb69c5d5f46d9de55fe5b4a7f4dcd3d3e
Bob Maerten <bob@levups.com>
2016-04-27 16:03:30 +0200

    Parameterize all known HTTP code

--- README.md
+++ README.md
@@ -4,16 +4,22 @@ A tiny webservice which returns a http error page from specified code in paramet
 
 ## Setup
 
-http-tester is a little sinatra application. To launch server, either `rackup config.ru -p 5000` or use provided `Procfile` with `foreman start`.
+http-tester is a little sinatra application. To launch server, either `rackup -p 5000` or use provided `Procfile` with `foreman start`.
 
 ## Usage
 
 ```bash
-$ curl -I http://localhost:5000/500
+$ curl http://localhost:5000/code/200
+OK
+
+$ curl http://localhost:5000/code/500
+Internal Server Error
+
+$ curl -I http://localhost:5000/code/500
 HTTP/1.1 500 Internal Server Error
 Content-Type: text/html;charset=utf-8
 X-XSS-Protection: 1; mode=block
 X-Content-Type-Options: nosniff
 X-Frame-Options: SAMEORIGIN
-Content-Length: 12
+Content-Length: 21
 ```
--- test/app_test.rb
+++ test/app_test.rb
@@ -5,6 +5,7 @@ require 'rack/test'
 
 require_relative '../app'
 
+# This is the main app tests
 class MainAppTest < Minitest::Test
   include Rack::Test::Methods
 
@@ -14,20 +15,28 @@ class MainAppTest < Minitest::Test
 
   def test_default_response
     get '/'
+    follow_redirect!
 
     assert last_response.ok?
-    assert last_response.body.include?('Hello, world!')
+    assert last_response.body.include?('OK')
+  end
+
+  def test_200_response
+    get '/code/200'
+
+    assert last_response.ok?
+    assert last_response.body.include?('OK')
   end
 
   def test_404_response
-    get '/404'
+    get '/code/404'
 
     assert last_response.not_found?
     assert last_response.body.include?('Not Found')
   end
 
   def test_500_response
-    get '/500'
+    get '/code/500'
 
     assert last_response.server_error?
     assert last_response.body.include?('Server Error')
@@ -44,6 +53,6 @@ class MainAppTest < Minitest::Test
     get '/infinite'
 
     assert last_response.status == 302
-    assert_equal "http://example.org/infinite", last_request.url
+    assert_equal 'http://example.org/infinite', last_request.url
   end
 end
