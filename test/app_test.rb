a7b9b6aaa0b39b2231665a51bbc504b2e7a9beef
Clément Joubert <clement.joubert@gmail.com>
2017-05-18 12:52:14 +0200

    refact: unify routes and behavior to follow DummyHTTP

--- test/app_test.rb
+++ test/app_test.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 ENV['RACK_ENV'] = 'test'
 
 # Local code coverage analysis
@@ -36,66 +38,69 @@ class MainAppTest < Minitest::Test
     follow_redirect!
 
     assert last_response.ok?
-    assert last_response.body.include?('OK')
+    assert_includes last_response.body, 'Hello World'
   end
 
   KNOWN_HTTP_CODES.each do |code, content|
     define_method "test_#{code}_response" do
       get "/code/#{code}"
 
-      assert last_response.status == code
-      assert last_response.body.include?(content) unless no_body_expected?(code)
+      assert_equal last_response.status, code
+      assert_includes(last_response.body, content) unless no_body_expected?(code)
     end
   end
 
   def test_slow_response
+    before_time = Time.now
     get '/slow'
+    after_time = Time.now
+    duration   = after_time.to_f - before_time.to_f
 
     assert last_response.ok?
-    assert last_response.body.include?('Hello, tired!')
-  end
-
-  def test_infinite_response
-    get '/infinite'
-
-    assert last_response.status == 302
-    assert_equal 'http://example.org/infinite', last_request.url
+    assert_includes last_response.body, 'Hello, tired!'
+    assert_operator duration, :>=, 10.0
   end
 
   def test_xml
     get '/xml'
 
     assert last_response.ok?
-    assert last_response.body.include?(SAMPLE_XML)
+    assert_includes last_response.body, SAMPLE_XML
+    assert_equal last_response.body.size, SAMPLE_XML.size
   end
 
   def test_html
     get '/html'
 
     assert last_response.ok?
-    assert last_response.body.include?(SAMPLE_HTML)
+    assert_includes last_response.body, SAMPLE_HTML
+    assert_equal last_response.body.size, SAMPLE_HTML.size
   end
 
   def test_json
     get '/json'
 
     assert last_response.ok?
-    assert last_response.body.include?(SAMPLE_JSON)
+    assert_includes last_response.body, SAMPLE_JSON
+    assert_equal last_response.body.size, SAMPLE_JSON.size
   end
 
   def test_text
     get '/text'
 
     assert last_response.ok?
-    assert last_response.body.include?(SAMPLE_TEXT)
+    assert_includes last_response.body, SAMPLE_TEXT
+    assert_equal last_response.body.size, SAMPLE_TEXT.size
   end
 
-  def test_local_redirect
-    get '/local-redirect'
+  def test_local_redirection_with_follow_redirect
+    get '/redirection/local'
     follow_redirect!
 
     assert last_response.ok?
-    assert last_response.body.include?('OK')
+    assert_equal last_request.url, 'http://example.org/html'
+    assert_includes last_response.body, 'Hello World'
+  end
   end
 
   private
